1fe730d086447eeddf117841681ad1a747d3a54d
Henrik Lien <henril13@uia.no>
2018-03-22 13:16:39 +0100

    Added an initializer.

---
+++ app/Data/ApplicationDbInitializer.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+using app.Data;
+using app.Models;
+
+namespace app.Data
+{
+    public static class ApplicationDbInitializer
+    {
+        public static void Initialize(ApplicationDbContext db, UserManager<ApplicationUser> um, RoleManager<IdentityRole> rm, bool isDevelopment)
+        {
+            if (!isDevelopment)
+            {
+                db.Database.EnsureCreated();
+                return;
+            }
+
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            // Add roles
+            var adminRole = new IdentityRole("Admin");
+            rm.CreateAsync(adminRole).Wait();
+
+            // Add users
+            var admin = new ApplicationUser { UserName = "admin@uia.no", Email = "admin@uia.no" };
+            um.CreateAsync(admin, "Password1.").Wait();
+            um.AddToRoleAsync(admin, "Admin");
+
+            var user = new ApplicationUser { UserName = "user@uia.no", Email = "user@uia.no" };
+            um.CreateAsync(user, "Password1.");
+
+            // Add dummy data here
+
+            db.SaveChanges();
+        }
+    }
+}
--- app/Program.cs
+++ app/Program.cs
@@ -5,8 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
+using app.Data;
+using app.Models;
 
 namespace app
 {
@@ -14,7 +19,21 @@ namespace app
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var env = services.GetRequiredService<IHostingEnvironment>();
+                var db = services.GetRequiredService<ApplicationDbContext>();
+                var um = services.GetRequiredService<UserManager<ApplicationUser>>();
+                var rm = services.GetRequiredService<RoleManager<IdentityRole>>();
+                
+                ApplicationDbInitializer.Initialize(db, um, rm, env.IsDevelopment());
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>

e0a6308ec24e3dc1d35705751f58ffa28dc32dac
Erik Sandø <eriksa12@student.uia.no>
2018-04-09 23:30:18 +0200

    Added reservation model and created API.

---
+++ app/Models/Reservation.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace app.Models
+{
+    public class Reservation
+    {
+        public Reservation() {}
+
+        public Reservation(int toolId, string userId, DateTime fromDate, DateTime toDate)
+        {
+            ToolId = toolId;
+            UserId = userId;
+            FromDate = fromDate;
+            ToDate = toDate;
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public int ToolId { get; set; }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        public DateTime FromDate { get; set; }
+
+        [Required]
+        public DateTime ToDate { get; set; }
+    }
+}

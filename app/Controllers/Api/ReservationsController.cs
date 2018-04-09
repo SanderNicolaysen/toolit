c895fee421b40c14d7d1f875414f1eb06038934e
Erik Sandø <eriksa12@student.uia.no>
2018-04-09 23:30:18 +0200

    Added reservation model and created API.

---
+++ app/Controllers/Api/ReservationsController.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using app.Data;
+using app.Models;
+
+namespace app.Controllers_Api
+{
+    [Produces("application/json")]
+    [Route("api/Reservations")]
+    public class ReservationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReservationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reservations
+        [HttpGet]
+        public IEnumerable<Reservation> GetReservation()
+        {
+            return _context.Reservation;
+        }
+
+        // GET: api/Reservations/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetReservation([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var reservation = await _context.Reservation.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(reservation);
+        }
+
+        // PUT: api/Reservations/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReservation([FromRoute] int id, [FromBody] Reservation reservation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != reservation.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reservation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reservations
+        [HttpPost]
+        public async Task<IActionResult> PostReservation([FromBody] Reservation reservation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Reservation.Add(reservation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReservation", new { id = reservation.Id }, reservation);
+        }
+
+        // DELETE: api/Reservations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReservation([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var reservation = await _context.Reservation.SingleOrDefaultAsync(m => m.Id == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reservation.Remove(reservation);
+            await _context.SaveChangesAsync();
+
+            return Ok(reservation);
+        }
+
+        private bool ReservationExists(int id)
+        {
+            return _context.Reservation.Any(e => e.Id == id);
+        }
+    }
+}

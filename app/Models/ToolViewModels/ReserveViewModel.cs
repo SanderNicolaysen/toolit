8ef5fff268fa377f0d7c88a2ecffa52966d7f279
eriksa12 <eriksa12@student.uia.no>
2018-04-10 19:18:59 +0200

    Merged with remote branch.

---
+++ app/Models/ToolViewModels/ReserveViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace app.Models.ToolViewModels
+{
+    public class ReserveViewModel
+    {
+        public Tool Tool;
+
+        public Reservation Reservation;
+    }
+}

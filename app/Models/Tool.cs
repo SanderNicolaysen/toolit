c3f332aacf2eaa4adedcaac76f80245157904099
Henrik Lien <henril13@student.uia.no>
2018-04-21 22:30:45 +0200

    Now if you press the check-out-button, the check-in-button becomes visible - and vice versa. It is stored in the database wether a tool is 'checked out' or not. Fixed some bugs.

--- app/Models/Tool.cs
+++ app/Models/Tool.cs
@@ -18,10 +18,15 @@ namespace app.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+<<<<<<< HEAD
         public string Image { get; set; }
         public string Thumbnail { get; set; }
         public int StatusId { get; set; }
         public Status Status { get; set; }
+=======
+        public string Status { get; set; }
+        public bool CheckedOut { get; set; }
+>>>>>>> Now if you press the check-out-button, the check-in-button becomes visible - and vice versa. It is stored in the database wether a tool is 'checked out' or not. Fixed some bugs.
         public List<Report> Reports { get; set; }
         public List<Alarm> Alarms { get; set; }
         public string Alias { get; set; }

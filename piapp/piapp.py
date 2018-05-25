4e9671a4f75437a59903bbc4e8ec3b10c95e0a62
Benjamin Lucas Oscar Sjølander <blsjol16@student.uia.no>
2018-05-25 10:58:52 +0000

    added lcd support

--- piapp/piapp.py
+++ piapp/piapp.py
@@ -1,19 +1,24 @@
 import requests
+from lcd_16x2 import *
 
+lcd_setup()
 apikey = ""
 
 with open("apikey.txt", "r") as f:
-    apikey = f.readline()
+    apikey = f.readline().strip()
 
 
 while True:
-    print(apikey)
-    userId = input("Please scan your card")
-    toolId = input("Please scan your tool")
-    r = requests.post('http://localhost:5000/api/checkout', json = {
+    lcd_string("ToolIt Solutions", LCD_LINE_1)
+    lcd_string("Please scan card", LCD_LINE_2)
+    userId = input("Please scan your card\n")
+    lcd_string("Please scan tool", LCD_LINE_2)
+    toolId = input("Please scan your tool\n")
+    r = requests.post('http://159.89.1.137/api/checkout', json = {
         'key': apikey,
         'userIdentifierCode': userId,
         'toolIdentifierCode': toolId
         })
+    lcd_string(r.text.strip("\""), LCD_LINE_2)
     print(r.text)
-    break
+
